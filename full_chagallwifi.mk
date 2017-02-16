1f67865ca2f55a64145de26e6e87bd25dc66d13b
DarkExistence <darraghmcgee20@gmail.com>
2017-02-16 22:57:51 +0000

    AICPify
    Change-Id: I4269256b7a0a8dae7bc86a0b46d7b02bc588505a

--- full_chagallwifi.mk
+++ full_chagallwifi.mk
@@ -1,23 +1,24 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common_tablet.mk)
+
 # Inherit from chagallwifi device
 $(call inherit-product, device/samsung/chagallwifi/device.mk)
 
-PRODUCT_NAME := full_chagallwifi
+PRODUCT_NAME := aicp_chagallwifi
 PRODUCT_DEVICE := chagallwifi
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := SM-T800
-
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
-
-PRODUCT_NAME := lineage_chagallwifi
-PRODUCT_DEVICE := chagallwifi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=SM-T800 \
     PRODUCT_NAME=chagallwifi \
     PRODUCT_DEVICE=chagallwifi \
     TARGET_DEVICE=chagallwifi
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Darragh McGee (DarkExistence)"
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo lineage_chagallwifi-userdebug
+add_lunch_combo aicp_chagallwifi-userdebug
