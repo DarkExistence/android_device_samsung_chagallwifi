4fb9b72f8f50515bc2f9db4aed01d7788deaaa36
deadman96385 <seanhoyt963@gmail.com>
2017-03-26 13:41:17 +0100

    Add gps and wifi configs from vendor

--- device.mk
+++ device.mk
@@ -16,11 +16,21 @@
 
 $(call inherit-product, device/samsung/chagall-klimt-common/device.mk)
 
+#Audio
 PRODUCT_COPY_FILES += \
-    device/samsung/chagallwifi/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
-
-PRODUCT_COPY_FILES += \
+    device/samsung/chagallwifi/configs/audio/audio_policy.conf:system/etc/audio_policy.conf\
     device/samsung/chagallwifi/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
+# GPS
+PRODUCT_COPY_FILES += \
+    device/samsung/chagallwifi/configs/gps/gps.conf:system/etc/gps.conf \
+    device/samsung/chagallwifi/configs/gps/gps.xml:system/etc/gps.xml
+
+# Wifi
+PRODUCT_COPY_FILES += \
+    device/samsung/chagallwifi/configs/wifi/wpa_supplicant.conf:system/etc/wpa_supplicant.conf \
+    device/samsung/chagallwifi/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wpa_supplicant_overlay.conf \
+    device/samsung/chagallwifi/configs/wifi/p2p_supplicant_overlay.conf:system/etc/p2p_supplicant_overlay.conf
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
